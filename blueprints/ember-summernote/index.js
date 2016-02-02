52d80669425baac89eaf7b1439eae47b695b75ad
Nate Amack <nate@amack.org>
2016-02-02 11:22:49 -0700

    not sure if this is the best way to import, but it works for now

--- blueprints/ember-summernote/index.js
+++ blueprints/ember-summernote/index.js
@@ -4,7 +4,20 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import('bower_components/summernote/dist/summernote.js');
-    app.import('bower_components/summernote/dist/summernote.css');
+    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap-theme.min.css');
+    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+    app.import(app.bowerDirectory + '/font-awesome/css/font-awesome.min.css');
+    app.import(app.bowerDirectory + '/summernote/dist/summernote.css');
+    app.import(app.bowerDirectory + '/summernote/dist/summernote.js');
+    app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.ttf', {
+      destDir: 'fonts'
+    });
+    app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff', {
+      destDir: 'fonts'
+    });
+    app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff2', {
+      destDir: 'fonts'
+    });
   }
 }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,21 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap-theme.min.css');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+  app.import(app.bowerDirectory + '/font-awesome/css/font-awesome.min.css');
+  app.import(app.bowerDirectory + '/summernote/dist/summernote.css');
+  app.import(app.bowerDirectory + '/summernote/dist/summernote.js');
+  app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.ttf', {
+    destDir: 'fonts'
+  });
+  app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff', {
+    destDir: 'fonts'
+  });
+  app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff2', {
+    destDir: 'fonts'
+  });
 
   /*
     This build file specifes the options for the dummy test app of this
