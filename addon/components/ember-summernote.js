5d463851d240de7943b418b84569868c750166b8
Nate Amack <nate@amack.org>
2016-02-01 17:34:35 -0700

    trying to access summernote library

--- addon/components/ember-summernote.js
+++ addon/components/ember-summernote.js
@@ -1,6 +1,14 @@
+/* global Summernote */
 import Ember from 'ember';
 import layout from '../templates/components/ember-summernote';
+// import Summernote from 'summernote';
+
+const { $ } = Ember;
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+
+  setupSummernote: Ember.on('didInsertElement', function() {
+    let summernote = new Summernote();
+  })
 });
