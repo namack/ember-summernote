a3374f224ed0d560e43f43096b89359fd689c0fa
Nate Amack <nate@amack.org>
2016-02-01 17:28:32 -0700

    use unminified version in development for now

--- blueprints/ember-summernote/index.js
+++ blueprints/ember-summernote/index.js
@@ -4,7 +4,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import('bower_components/summernote/dist/summernote.min.js');
+    app.import('bower_components/summernote/dist/summernote.js');
     app.import('bower_components/summernote/dist/summernote.css');
   }
 }
